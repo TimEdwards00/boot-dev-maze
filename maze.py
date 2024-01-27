94938fef537fce54648e477eb94266845329ff72
TimEdwards00 <91420130+TimEdwards00@users.noreply.github.com>
2024-01-27 20:33:48 +0000

    drew cell

--- maze.py
+++ maze.py
@@ -53,15 +53,46 @@ class Line:
         )
         canvas.pack()
 
+# cell class, 
+class Cell:
+    def __init__(self, x1, x2, y1, y2, win):
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+
+        self._x1 = x1
+        self._x2 = x2
+        self._y1 = y1
+        self._y2 = y2
+
+        self._win = win
+
+    def draw(self):
+        if self.has_left_wall:
+            temp_line = Line(Point(self._x1,self._y1),Point(self._x1,self._y2))
+            self._win.draw_line(temp_line, "red")
+
+        if self.has_right_wall:
+            temp_line = Line(Point(self._x2,self._y1),Point(self._x2,self._y2))
+            self._win.draw_line(temp_line, "red")
+
+        if self.has_top_wall:
+            temp_line = Line(Point(self._x1,self._y1),Point(self._x2,self._y1))
+            self._win.draw_line(temp_line, "red")
+
+        if self.has_bottom_wall:
+            temp_line = Line(Point(self._x1,self._y2),Point(self._x2,self._y2))
+            self._win.draw_line(temp_line, "red")
+
+
+
 
 def main():
     win = Window(800, 600)
 
-    line1 = Line(Point(10,10),Point(60,100))
-    win.draw_line(line1, "black")
-
-    line2 = Line(Point(10,310),Point(50,10))
-    win.draw_line(line2, "red")
+    test_cell = Cell(100,200,100,200,win)
+    test_cell.draw()
 
 
     win.wait_for_close()
